98d211b9236124decf7d9ea46c0ce499f6ec685a
Skylor R Schermer <skyschermer@gmail.com>
2016-02-16 21:54:55 -0600

    DOCS: Added licence and module-level doc dividers.

--- src/utilities.rs
+++ src/utilities.rs
@@ -19,8 +19,12 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
+//
+////////////////////////////////////////////////////////////////////////////////
+//!
 //! Defines general purpose functions for rampeditor use.
+//!
+////////////////////////////////////////////////////////////////////////////////
 use std::f32;
 
 
