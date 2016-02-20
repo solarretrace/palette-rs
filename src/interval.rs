15ecbd90b07d3c001abe62e661dab0ce24023a42
Skylor R Schermer <skyschermer@gmail.com>
2016-02-20 15:04:36 -0600

    FIX: Attempted to correct intersection function.

--- src/interval.rs
+++ src/interval.rs
@@ -34,33 +34,165 @@ use std::cmp::Ord;
 ///
 /// Determines the type of an interval's boundary.
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Hash, Clone, Copy)]
-pub enum Boundary<T> {
+pub enum Boundary<T> where T: PartialOrd + PartialEq + Clone {
     /// The boundary includes the point.
     Include(T),
     /// The boundary excludes the point.
     Exclude(T),
 }
 
-impl<T> Boundary<T> {
+impl<T> Boundary<T> where T: PartialOrd + PartialEq + Clone {
     /// Returns whether the boundary includes its point.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::Boundary;
+    ///
+    /// let b1 = Boundary::Include(0);
+    /// let b2 = Boundary::Exclude(1);
+    /// 
+    /// assert!(b1.is_closed());
+    /// assert!(!b2.is_closed());
+    /// ```
     #[inline]
-    pub fn is_inclusive(&self) -> bool {
+    pub fn is_closed(&self) -> bool {
         match self {
             &Boundary::Include(..) => true,
             &Boundary::Exclude(..) => false
         }
     }
 
-    /// Returns whether the boundary excludes its point.
+    /// Returns whether the boundary excludes its point. 
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::Boundary;
+    ///
+    /// let b1 = Boundary::Include(0);
+    /// let b2 = Boundary::Exclude(1);
+    /// 
+    /// assert!(!b1.is_open());
+    /// assert!(b2.is_open());
+    /// ```
     #[inline]
-    pub fn is_exclusive(&self) -> bool {
-        !self.is_inclusive()
+    pub fn is_open(&self) -> bool {
+        !self.is_closed()
+    }
+
+    /// Returns the intersect of the given boundaries, or the lowest one if they
+    /// are not at the same point.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::Boundary;
+    ///
+    /// let b1 = Boundary::Include(0);
+    /// let b2 = Boundary::Exclude(0);
+    /// 
+    /// assert_eq!(b1.intersect_or_least(&b2), b2);
+    /// ```
+    pub fn intersect_or_least(&self, other: &Self) -> Self {
+        if **self == **other {
+            if self.is_closed() && other.is_closed() {
+                self.clone()
+            } else {
+                Boundary::Exclude((**self).clone())
+            }
+        } else if **self < **other {
+            self.clone()
+        } else {
+            other.clone()
+        }
+    }
+
+    /// Returns the intersect of the given boundaries, or the greatest one if 
+    /// they are not at the same point.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::Boundary;
+    ///
+    /// let b1 = Boundary::Include(0);
+    /// let b2 = Boundary::Exclude(0);
+    /// 
+    /// assert_eq!(b1.intersect_or_greatest(&b2), b2);
+    /// ```
+    pub fn intersect_or_greatest(&self, other: &Self) -> Self {
+        if **self == **other {
+            if self.is_closed() && other.is_closed() {
+                self.clone()
+            } else {
+                Boundary::Exclude((**self).clone())
+            }
+        } else if **self > **other {
+            self.clone()
+        } else {
+            other.clone()
+        }
+    }
+
+    /// Returns the union of the given boundaries, or the lowest one if they are
+    /// not at the same point.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::Boundary;
+    ///
+    /// let b1 = Boundary::Include(0);
+    /// let b2 = Boundary::Exclude(0);
+    /// 
+    /// assert_eq!(b1.union_or_least(&b2), b1);
+    /// ```
+    pub fn union_or_least(&self, other: &Self) -> Self {
+        if **self == **other {
+            if self.is_open() && other.is_open() {
+                self.clone()
+            } else {
+                Boundary::Include((**self).clone())
+            }
+        } else if **self < **other {
+            self.clone()
+        } else {
+            other.clone()
+        }
+    }
+
+    /// Returns the union of the given boundaries, or the greatest one if they 
+    /// are not at the same point.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::Boundary;
+    ///
+    /// let b1 = Boundary::Include(0);
+    /// let b2 = Boundary::Exclude(0);
+    /// 
+    /// assert_eq!(b1.union_or_greatest(&b2), b1);
+    /// ```
+    pub fn union_or_greatest(&self, other: &Self) -> Self {
+        if **self == **other {
+            if self.is_open() && other.is_open() {
+                self.clone()
+            } else {
+                Boundary::Include((**self).clone())
+            }
+        } else if **self > **other {
+            self.clone()
+        } else {
+            other.clone()
+        }
     }
 }
 
 // Implemented to prevent having to match on the Boundary enum to use its 
 // contents.
-impl<T> Deref for Boundary<T> {
+impl<T> Deref for Boundary<T> where T: PartialOrd + PartialEq + Clone {
     type Target = T;
     fn deref(&self) -> &Self::Target {
         match *self {
@@ -77,14 +209,14 @@ impl<T> Deref for Boundary<T> {
 /// A contiguous range of the type T, which may include or exclude either 
 /// boundary.
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-pub struct Interval<T> where T: PartialOrd + Clone {
+pub struct Interval<T> where T: PartialOrd + PartialEq + Clone {
     /// The start of the range.
     start: Boundary<T>,
     /// The end of the range.
     end: Option<Boundary<T>>
 }
 
-impl <T> Interval<T> where T: PartialOrd + Clone  {
+impl <T> Interval<T> where T: PartialOrd + PartialEq + Clone  {
     /// Creates a new interval from the given boundaries.
     ///
     /// # Example
@@ -103,7 +235,8 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// If the arguments are out of order, they will be swapped:
     ///
     /// ```rust
-    /// # use rampeditor::{Boundary, Interval};
+    /// use rampeditor::{Boundary, Interval};
+    ///
     /// let l = Boundary::Include(12);
     /// let r = Boundary::Include(16);
     /// let int = Interval::new(r, Some(l));
@@ -113,7 +246,7 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// ```
     pub fn new(start: Boundary<T>, end: Option<Boundary<T>>) -> Self {
         if let Some(end_bound) = end {
-            if *end_bound <= *start {
+            if *end_bound < *start {
                 Interval {start: end_bound, end: Some(start)}
             } else {
                 Interval {start: start, end: Some(end_bound)}
@@ -128,13 +261,14 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// # Example
     ///
     /// ```rust
-    /// # use rampeditor::Interval;
+    /// use rampeditor::Interval;
+    ///
     /// let int = Interval::open(0, 2);
     /// 
     /// assert_eq!(int.left_point(), 0);
-    /// assert!(!int.left_bound().is_inclusive());
+    /// assert!(!int.left_bound().is_closed());
     /// assert_eq!(int.right_point(), 2);
-    /// assert!(!int.right_bound().is_inclusive());
+    /// assert!(!int.right_bound().is_closed());
     /// ```
     pub fn open(start: T, end: T) -> Self {
         Interval::new(
@@ -148,13 +282,14 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// # Example
     ///
     /// ```rust
-    /// # use rampeditor::Interval;
+    /// use rampeditor::Interval;
+    ///
     /// let int = Interval::closed(0, 2);
     /// 
     /// assert_eq!(int.left_point(), 0);
-    /// assert!(int.left_bound().is_inclusive());
+    /// assert!(int.left_bound().is_closed());
     /// assert_eq!(int.right_point(), 2);
-    /// assert!(int.right_bound().is_inclusive());
+    /// assert!(int.right_bound().is_closed());
     /// ```
     pub fn closed(start: T, end: T) -> Self {
         Interval::new(
@@ -168,13 +303,14 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// # Example
     ///
     /// ```rust
-    /// # use rampeditor::Interval;
+    /// use rampeditor::Interval;
+    ///
     /// let int = Interval::left_open(0, 2);
     /// 
     /// assert_eq!(int.left_point(), 0);
-    /// assert!(!int.left_bound().is_inclusive());
+    /// assert!(!int.left_bound().is_closed());
     /// assert_eq!(int.right_point(), 2);
-    /// assert!(int.right_bound().is_inclusive());
+    /// assert!(int.right_bound().is_closed());
     /// ```
     pub fn left_open(start: T, end: T) -> Self {
         Interval::new(
@@ -188,14 +324,14 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// # Example
     ///
     /// ```rust
-    /// # use rampeditor::Interval;
+    /// use rampeditor::Interval;
     ///
     /// let int = Interval::right_open(0, 2);
     /// 
     /// assert_eq!(int.left_point(), 0);
-    /// assert!(int.left_bound().is_inclusive());
+    /// assert!(int.left_bound().is_closed());
     /// assert_eq!(int.right_point(), 2);
-    /// assert!(!int.right_bound().is_inclusive());
+    /// assert!(!int.right_bound().is_closed());
     /// ```
     pub fn right_open(start: T, end: T) -> Self {
         Interval::new(
@@ -206,6 +342,16 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
 
     /// Returns the leftmost (least) boundary point of the interval. Note that 
     /// this point may not be in the interval if the interval is left-open.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::Interval;
+    ///
+    /// let int = Interval::open(0, 2);
+    /// 
+    /// assert_eq!(int.left_point(), 0);
+    /// ```
     #[inline]
     pub fn left_point(&self) -> T {
         (*self.start).clone()
@@ -214,6 +360,16 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// Returns the rightmost (greatest) boundary point of the interval. Note 
     /// that this point may not be in the interval if the interval is 
     /// right-open.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::Interval;
+    ///
+    /// let int = Interval::open(0, 2);
+    /// 
+    /// assert_eq!(int.right_point(), 2);
+    /// ```
     #[inline]
     pub fn right_point(&self) -> T {
         if let Some(ref end_bound) = self.end {
@@ -224,12 +380,32 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     }
 
     /// Returns the left (least) boundary of the interval.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::{Interval, Boundary};
+    ///
+    /// let int = Interval::open(0, 2);
+    /// 
+    /// assert_eq!(int.left_bound(), Boundary::Exclude(0));
+    /// ```
     #[inline]
     pub fn left_bound(&self) -> Boundary<T> {
         self.start.clone()
     }
 
     /// Returns the right (greatest) boundary of the interval.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use rampeditor::{Interval, Boundary};
+    ///
+    /// let int = Interval::open(0, 2);
+    /// 
+    /// assert_eq!(int.right_bound(), Boundary::Exclude(2));
+    /// ```
     #[inline]
     pub fn right_bound(&self) -> Boundary<T> {
         if let Some(ref end_bound) = self.end {
@@ -277,7 +453,7 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     #[inline]
     pub fn is_empty(&self) -> bool {
         self.left_bound() == self.right_bound() 
-            && self.left_bound().is_exclusive()
+            && self.left_bound().is_open()
     }
 
     /// Returns whether the given point is included in the interval.
@@ -287,26 +463,15 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// ```rust
     /// use rampeditor::{Interval, Boundary};
     /// let int = Interval::right_open(0.0, 2.0);
-    /// assert!(!int.contains(&-1.34));
-    /// assert!(!int.contains(&-0.001));
     /// assert!(int.contains(&0.0));
-    /// assert!(int.contains(&0.001));
     /// assert!(int.contains(&1.0));
-    /// assert!(int.contains(&1.9999));
     /// assert!(!int.contains(&2.0));
     /// ```
     #[inline]
     pub fn contains(&self, point: &T) -> bool {
         *point > self.left_point() && *point < self.right_point()
-            || *point == self.left_point() && self.left_bound().is_inclusive()
-            || *point == self.right_point() && self.right_bound().is_inclusive()
-
-        // if let Some(ref end_bound) = self.end {
-        //     (*point >= *self.start && *point < **end_bound) || 
-        //     (end_bound.is_inclusive() && *point == **end_bound)
-        // } else {
-        //     self.start.is_inclusive() && *point == *self.start
-        // }
+            || *point == self.left_point() && self.left_bound().is_closed()
+            || *point == self.right_point() && self.right_bound().is_closed()
     }
 
     /// Returns the set union of the interval with the given interval. Note that
@@ -319,7 +484,46 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     /// Returns the set intersection of the interval with the given interval,
     /// or None if the intervals do not overlap.
     pub fn intersect(&self, other: &Self) -> Option<Self> {
-        unimplemented!()
+        // Check if either one is empty.
+        if self.is_empty() || other.is_empty() {
+            return None;
+        }
+
+        // Check if they're the same set.
+        if self == other {
+            return Some(self.clone());
+        }
+
+        // a:[], b:{}
+        let (a, b) = if self.left_point() <= other.left_point() {
+            (self, other)
+        } else {
+            (other, self)
+        };
+
+        
+        if a.right_point() < b.left_point() {
+            // []_{}    -> None
+            // [_]_{}   -> None
+            // []_{_}   -> None
+            // [_]_{_}  -> None
+            None
+        } else if a.right_point() == b.left_point() {
+            // [_]{_}   -> ]{ or None
+            if a.right_bound().is_closed() || b.left_bound().is_closed() {
+                Some(Interval::new(
+                    Boundary::Include(a.right_point()), 
+                    None
+                ))
+            } else {
+                None
+            }
+        } else {
+            Some(Interval::new(
+                 a.left_bound().intersect_or_greatest(&b.left_bound()),
+                 Some(a.right_bound().intersect_or_least(&b.right_bound()))
+            ))
+        }
     }
 
     /// Returns the interval with all the points in the intersection with the 
@@ -340,7 +544,11 @@ impl <T> Interval<T> where T: PartialOrd + Clone  {
     }
 }
 
-impl <'a, T> Interval<T> where T: PartialOrd + Clone + 'a, &'a T: Sub  {
+impl <'a, T> Interval<T> 
+    where 
+        T: PartialOrd + PartialEq + Clone + 'a, 
+        &'a T: Sub  
+{
     /// Returns the width of the interval.
     ///
     /// # Examples
@@ -361,12 +569,68 @@ impl <'a, T> Interval<T> where T: PartialOrd + Clone + 'a, &'a T: Sub  {
     /// assert_eq!(int.width(), 0.0);
     /// ```
     pub fn width(&'a self) -> <&'a T as Sub>::Output 
-        where T: PartialOrd, <&'a T as Sub>::Output: Default 
+        where <&'a T as Sub>::Output: Default 
     {
         if let Some(ref end_bound) = self.end {
             &**end_bound - &*self.start
         } else {
             Default::default()
         }
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Test Module
+////////////////////////////////////////////////////////////////////////////////
+#[cfg(test)]
+mod tests {
+    use super::{Boundary, Interval};
+
+    /// Tests the Interval::intersect function.
+    #[test]
+    fn interval_contains() {
+        let int = Interval::right_open(0.0, 2.0);
+        assert!(!int.contains(&-1.34));
+        assert!(!int.contains(&-0.001));
+        assert!(int.contains(&0.0));
+        assert!(int.contains(&0.001));
+        assert!(int.contains(&1.0));
+        assert!(int.contains(&1.9999));
+        assert!(!int.contains(&2.0));
+    }
+
+    /// Tests the Interval::intersect function.
+    #[test]
+    fn interval_intersect() {
+        let o: fn(f32, f32) -> Interval<f32> = Interval::open;
+        let c: fn(f32, f32) -> Interval<f32> = Interval::closed;
+        let lo: fn(f32, f32) -> Interval<f32> = Interval::left_open;
+        let ro: fn(f32, f32) -> Interval<f32> = Interval::right_open;
+
+        // Open overlapping
+        assert_eq!( o(1.0, 2.0).intersect(& o(1.0, 2.0)), Some( o(1.0, 2.0)));
+        assert_eq!( o(1.0, 2.0).intersect(&lo(1.0, 2.0)), Some( o(1.0, 2.0)));
+        assert_eq!( o(1.0, 2.0).intersect(&ro(1.0, 2.0)), Some( o(1.0, 2.0)));
+        assert_eq!( o(1.0, 2.0).intersect(& c(1.0, 2.0)), Some( o(1.0, 2.0)));
+
+        // Closed overlapping
+        assert_eq!( c(1.0, 2.0).intersect(& o(1.0, 2.0)), Some( o(1.0, 2.0)));
+        assert_eq!( c(1.0, 2.0).intersect(&lo(1.0, 2.0)), Some(lo(1.0, 2.0)));
+        assert_eq!( c(1.0, 2.0).intersect(&ro(1.0, 2.0)), Some(ro(1.0, 2.0)));
+        assert_eq!( c(1.0, 2.0).intersect(& c(1.0, 2.0)), Some( c(1.0, 2.0)));
+        
+        // Open left-half overlapping
+        assert_eq!( o(1.0, 2.0).intersect(& o(1.0, 1.5)), Some( o(1.0, 1.5)));
+        assert_eq!( o(1.0, 2.0).intersect(&lo(1.0, 1.5)), Some(lo(1.0, 1.5)));
+        assert_eq!( o(1.0, 2.0).intersect(&ro(1.0, 1.5)), Some( o(1.0, 1.5)));
+        assert_eq!( o(1.0, 2.0).intersect(& c(1.0, 1.5)), Some(lo(1.0, 1.5)));
+
+        // Close left-half overlapping
+        assert_eq!( c(1.0, 2.0).intersect(& o(1.0, 1.5)), Some( o(1.0, 1.5)));
+        assert_eq!( c(1.0, 2.0).intersect(&lo(1.0, 1.5)), Some(lo(1.0, 1.5)));
+        assert_eq!( c(1.0, 2.0).intersect(&ro(1.0, 1.5)), Some(ro(1.0, 1.5)));
+        assert_eq!( c(1.0, 2.0).intersect(& c(1.0, 1.5)), Some( c(1.0, 1.5)));
     }
 }
